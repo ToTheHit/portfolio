a828c3559d247197a9a18d32f93edfce7e2a8ee2
ToTheHit <dmtshkv@gmail.com>
2020-04-26 20:34:48 +0300

    Hot fixes

--- brew/webpack.prod.js
+++ brew/webpack.prod.js
@@ -1,40 +1,43 @@
 const merge = require('webpack-merge');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 const extractLESS = new ExtractTextPlugin('stylesheets/[name]-two.css');
 
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "less-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "/styles/[name].css",
-            chunkFilename: "/styles/[id].css"
-        }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                PUBLIC_URL: JSON.stringify('') // Paste here part of Github Pages URL after /
-            },
-        })
-    ]
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '/styles/[name].css',
+      chunkFilename: '/styles/[id].css',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        PUBLIC_URL: JSON.stringify(''), // Paste here part of Github Pages URL after /
+      },
+    }),
+  ],
 });
